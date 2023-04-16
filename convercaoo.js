4a205d850d0a06e83249a3024da25203d7001812
Mart1ns <pauloeduardomartins700305@gmail.com>
2023-04-16 14:08:57 -0300

    novos aqruivos do curso

---
+++ convercaoo.js
@@ -0,0 +1,6 @@
+// converter tipos
+
+let stringuinha = '123';
+let numberzinho = 1;
+
+console.log(numberzinho + Number(stringuinha))
---
+++ falsy-truthy.js
@@ -0,0 +1,5 @@
+console.log(0 == false);
+
+let minhaVar; // undefined
+let varNull = null
+console.log( typeof varNull)
